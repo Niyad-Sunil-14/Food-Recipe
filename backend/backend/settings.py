6dd37f2b1790884bf378f4e6b69f834ac56f1aa2
Niyad <niyadsystem@gmail.com>
2025-08-25 17:12:44 +0530

    Test12

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://food-recipe-backend-hntt.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['food-recipe-backend-hntt.onrender.com','food-recipe-virid.vercel.app', '127.0.0.1']
 
 
 # Application definition
